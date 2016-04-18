a56d5a9c09455c7e2256a81b99dfeb33f914f895
xulu <gordonzu@outlook.com>
2016-04-18 16:09:52 -0400

    Implement error.three check

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -4,17 +4,32 @@ var validator = require('../lib/validator');
 
 describe('A Validator', function () {
   
-  it('will return no errors for valid numbers', function  () {
-    expect(validator(3)).to.be.empty;
-  });
+    it('will return no errors for valid numbers', function  () {
+        expect(validator(7)).to.be.empty;
+    });
 
-  it('will return error.nonpositive for not strictly positive numbers like 0', function  () {
-    expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
-  });
-  
-  it('will return error.nonpositive for not strictly positive numbers like -2', function  () {
-    expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
-  });
+    describe('will return error.nonpositive for non-positive numbers:', function () {
+
+        it('like 0', function  () {
+            expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+        });
+        
+        it('like -2', function  () {
+            expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+        });
+    });
+    
+    describe('will return error.three for divisible by three numbers:', function () {
+
+        it('like 3', function  () {
+            expect(validator(3)).to.be.deep.equal(['error.three']);
+        });
+        
+        it('like 6', function  () {
+            expect(validator(6)).to.be.deep.equal(['error.three']);
+        });
+    });
 });
+          
 
 
